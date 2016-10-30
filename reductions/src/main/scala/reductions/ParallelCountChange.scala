04f2818934cdfc00e22e28ff614ec1e531034e35
Gerd Reiss <gerdreiss@jscriptive.com>
2016-10-30 18:08:04 +0100

    minor fixes

--- reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -57,23 +57,23 @@ object ParallelCountChange {
     * specified list of coins for the specified amount of money.
     */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    if (threshold(money, coins)) {
+    if (threshold(money, coins) || money <= 0 || coins.isEmpty) {
       countChange(money, coins)
     } else {
-      val (l, r) = parallel(
+      val (left, right) = parallel(
         parCountChange(money - coins.head, coins, threshold),
         parCountChange(money, coins.tail, threshold))
-      l + r
+      left + right
     }
   }
 
   /** Threshold heuristic based on the starting money. */
   def moneyThreshold(startingMoney: Int): Threshold = (money, coins) =>
-    money <= 0 || money <= (2 * startingMoney) / 3
+    money <= (2 * startingMoney) / 3
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold = (money, coins) =>
-    coins.isEmpty || coins.size <= (totalCoins * 2) / 3
+    coins.size <= (totalCoins * 2) / 3
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
   def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = (money, coins) =>
